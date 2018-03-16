82d2f12e834c8693aff3f0a86d7d610941e8d4f6
Bit <Bit@Bitcamp>
2018-03-16 18:57:18 +0900

    d0316

--- work/src/apiEx/string/StringTest03.java
+++ work/src/apiEx/string/StringTest03.java
@@ -7,21 +7,27 @@ public class StringTest03 {
 		String name = "012ABCdef";
 		String hi = "Hi !";
 
+		// concat(String str) : 지정된 문자열을이 문자열의 끝에 연결합니다.
 		String s1 = hi.concat(name);
 		System.out.println(s1);
 
+		// substring(int beginIndex) : 이 문자열의 하위 문자열 인 새 문자열을 반환합니다.
 		String s2 = s1.substring(7);
 		System.out.println(s2);
 
+		// indexOf(String str) : 이 캐릭터 라인 내에서, 지정된 문자가 최초로 출현하는 위치의 인덱스를 돌려줍니다.
 		int a = name.indexOf("ABC");
 		System.out.println(a);
 		System.out.println(name.substring(a));
 
+		
 		int b = name.indexOf("ABC");
 		System.out.println(b);
 		System.out.println(name.substring(b));
+		// 	substring(int beginIndex, int endIndex) : 이 문자열의 하위 문자열 인 새 문자열을 반환합니다.
 		System.out.println(name.substring(b, 5));
 
+		// 이 객체가 나타내는 문자 순서에서 문자가 처음 나오는 색인. 문자가없는 경우 -1.
 		int c = name.indexOf("z");
 		System.out.println(c);
 
--- work/src/collectionEx/list/vector/VectorTest01.java
+++ work/src/collectionEx/list/vector/VectorTest01.java
@@ -7,16 +7,19 @@ public class VectorTest01 {
 	public static void main(String[] args) {
 
 		// Vector 생성 : API의 생성자를 확인, 인자로 전달된 10, 10 의미 확인
+		// Vector(int initialCapacity, int capacityIncrement) : 지정된 초기 용량 및 증분을 사용하여 빈 벡터를 구성합니다.
 		Vector vector = new Vector(10, 10);
 
 		// Vector에 Object 저장 ==> add(Object obj) : Object를 인자로 받는다는 의미는?
 		String s1 = new String("1.홍");
+		// add(E e) : 지정된 요소를 Vector의 말미에 추가합니다.
 		vector.add(s1);
 		vector.add(new String("2.동"));
 		vector.add("3.님 안녕하세요");
 
 		// Vector 저장된 값을 출력 :: vector.capacity() 무엇을 return 하는가 ...
 		for (int i = 0; i < vector.size(); i++) {
+			// 	elementAt(int index) : 지정된 인덱스의 컴퍼넌트를 돌려줍니다.
 			Object obj = vector.elementAt(i);
 			String s = (String) obj;
 			// String s = (String)vector.elementAt(i);
@@ -24,18 +27,21 @@ public class VectorTest01 {
 			// System.out.print((String)vector.elementAt(i));
 		}
 
+		// 	insertElementAt(E obj, int index) : 이 벡터의 지정된 인덱스에있는 구성 요소로 지정된 객체를 삽입합니다.
 		vector.insertElementAt("4.길", 1);
 		for (int i = 0; i < vector.size(); i++) {
 			System.out.print((String) vector.elementAt(i));
 		}
 
 		System.out.println("\n ==> API 확인");
+		// 	setElementAt(E obj, int index) : 이 벡터의 지정된 인덱스에있는 컴포넌트를 지정된 객체로 설정합니다.
 		vector.setElementAt("5.홍길순", 3);
 		for (int i = 0; i < vector.size(); i++) {
 			System.out.print((String) vector.elementAt(i));
 		}
 
 		System.out.println("\n ==> API 확인");
+		// removeElementAt(int index) : 지정된 인덱스에있는 구성 요소를 삭제합니다.
 		vector.removeElementAt(3);
 		for (int i = 0; i < vector.size(); i++) {
 			System.out.print((String) vector.elementAt(i));
